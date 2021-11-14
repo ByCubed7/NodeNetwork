ea03835ac8a6542b913370310b5d7ff086a66a90
BuildTools <38486990+ByCubed7@users.noreply.github.com>
2021-11-14 23:38:01 +0000

    Reworked the NodeNetwork class
    - Rewrote NodeNetwork class
    - Removed the Node class

--- Scripts/NodeNetwork.cs
+++ Scripts/NodeNetwork.cs
@@ -8,11 +8,11 @@ namespace ByCubed7.Pathfinding
 {
     public class NodeNetwork
     {
-        public Dictionary<(int, int), Node> nodes;
+        public Dictionary<(int, int), List<(int, int)>> nodes;
 
         public NodeNetwork()
         {
-            nodes = new Dictionary<(int, int), Node>();
+            nodes = new Dictionary<(int, int), List<(int, int)>>();
             //path = PathfindAStar(nodes[0,6], nodes[14,0]);
         }
 
@@ -21,25 +21,13 @@ namespace ByCubed7.Pathfinding
             BoundsInt bounds = tilemap.cellBounds;
             TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
 
-            nodes = new Dictionary<(int, int), Node>();
-            //nodes = new Node[bounds.size.x, bounds.size.y)];
-
-            Vector3 offset = (Vector3)tilemap.origin * tilemap.cellSize.x;
-            Vector3 anchor = (Vector3)tilemap.tileAnchor * tilemap.cellSize.x;
+            nodes = new Dictionary<(int, int), List<(int, int)>>();
 
             for (int x = 0; x < bounds.size.x; x++) {
                 for (int y = 0; y < bounds.size.y; y++) {
                     Tile tile = (Tile)allTiles[x + y * bounds.size.x];
-                    if (tile != null) {
-                        //Debug.Log("x:" + x + " y:" + y + " tile: " + tile);
-                    } else {
-                        //Debug.Log("x:" + x + " y:" + y + " tile: (null)");
-                        Vector3 worldPosition = tilemap.CellToWorld(new Vector3Int(x, y, 0));
-                        Vector3 tilePosition = worldPosition + offset + anchor;
-                        nodes[(x, y)] = new Node(
-                            new Vector2Int(x, y),
-                            new Vector2(tilePosition.x, tilePosition.y)
-                        );
+                    if (tile == null) {
+                        AddNode(x, y);
                     }
                 }
             }
@@ -47,56 +35,46 @@ namespace ByCubed7.Pathfinding
             // link neighbours
             for (int x = 0; x < bounds.size.x; x++) {//nodes.GetLength(0)
                 for (int y = 0; y < bounds.size.y; y++) {
-                    Node currentNode = GetNodeAt(x, y);
+                    List<(int, int)> currentNode = GetNode(x, y);
 
                     if (currentNode == null) continue;
 
-                    if (x + 1 < bounds.size.x) currentNode.AddNeighbour(GetNodeAt(x + 1, y));
-                    if (y + 1 < bounds.size.y) currentNode.AddNeighbour(GetNodeAt(x, y + 1));
-                    if (x > 0) currentNode.AddNeighbour(GetNodeAt(x - 1, y));
-                    if (y > 0) currentNode.AddNeighbour(GetNodeAt(x, y - 1));
+                    if (x + 1 < bounds.size.x) AddNeighbour((x, y), (x + 1, y));
+                    if (y + 1 < bounds.size.y) AddNeighbour((x, y), (x, y + 1));
+                    if (x > 0) AddNeighbour((x, y), (x - 1, y));
+                    if (y > 0) AddNeighbour((x, y), (x, y - 1));
                 }
             }
         }
 
-        public Node GetNodeAt(int x, int y)
+        public List<(int, int)> PathfindAStar((int, int) nodeStart, (int, int) nodeTarget)
         {
-            if (nodes.ContainsKey((x, y)))
-                return nodes[(x, y)];
-            return null;
-        }
-
-        // Converts the dictionary node path to a list of nodes
-        private List<Node> MakePath(Dictionary<Node, Node> nodes, Node targetNode)
-        {
-            Node current = targetNode;
-            List<Node> path = new List<Node>() {current};
-
-            List<Node> nodeKeys = new List<Node>(nodes.Keys);
-            while (nodeKeys.Contains(current))
+            // First, check both the starting position and ending position exist
+            if (!nodes.ContainsKey(nodeStart))
             {
-                current = nodes[current];
-                path.Add(current);
+                Debug.LogWarning("[Node Network] Starting path position is not accessible.");
+                return new List<(int, int)>();
             }
-            return path;
-        }
+            if (!nodes.ContainsKey(nodeTarget))
+            {
+                Debug.LogWarning("[Node Network] Target path position is not accessible.");
+                return new List<(int, int)>();
+            }
 
-        public List<Node> PathfindAStar(Node nodeStart, Node nodeTarget)
-        {
             // The set of discovered nodes that may need to be (re-)expanded.
             // Initially, only the start node is known.
             // "This is usually implemented as a min-heap or priority queue rather than a hash-set."
-            List<Node> nodesToExpand = new List<Node>();
+            List<(int, int)> nodesToExpand = new List<(int, int)>();
 
             // cameFrom[node] is the node immediately preceding it on path from start
-            Dictionary<Node, Node> cameFrom = new Dictionary<Node, Node>();
+            Dictionary<(int, int), (int, int)> cameFrom = new Dictionary<(int, int), (int, int)>();
 
             // gScore[node] is the cost of the cheapest path from start to node
-            Dictionary<Node, int> gScore = new Dictionary<Node, int>();
+            Dictionary<(int, int), int> gScore = new Dictionary<(int, int), int>();
 
             // fScore[node] = gScore[node] + Score(node). fScore[node] is the current guess as to
             // how short a path from start to finish can be if it goes through the node.
-            Dictionary<Node, int> fScore = new Dictionary<Node, int>();
+            Dictionary<(int, int), int> fScore = new Dictionary<(int, int), int>();
 
             // Add the starting node
             nodesToExpand.Add(nodeStart);
@@ -111,29 +89,35 @@ namespace ByCubed7.Pathfinding
                 nodesToExpand.Sort((nodeA, nodeB) => ((int)fScore[nodeA] - (int)fScore[nodeB]));
 
                 // The node in nodesToExpand having the lowest fScore value
-                Node current = nodesToExpand[0];
+                (int, int) current = nodesToExpand[0];
 
                 if (current == nodeTarget)
                     return MakePath(cameFrom, nodeTarget);
 
                 nodesToExpand.Remove(current);
 
-                List<Node> currentNeighbours = current.neighbour;
-                for (int i = 0; i < current.neighbour.Count; i++)
+                List<(int, int)> neighbours = GetNode(current);
+
+                if (neighbours == null) {
+                    Debug.LogWarning("[Node Network] Can not find accessible path.");
+                    return new List<(int, int)>();
+                }
+
+                for (int i = 0; i < neighbours.Count; i++)
                 {
-                    Node neighbor = current.neighbour[i];
-                    // Distance(current, neighbor) is the weight of the edge from current to neighbor
+                    (int, int) neighbour = neighbours[i];
+                    // Distance(current, neighbour) is the weight of the edge from current to neighbour
                     // tempGScore is the distance from start to the neighbor through current
-                    int tempGScore = gScore[current] + (int)current.Distance(neighbor);
+                    int tempGScore = gScore[current] + Distance(current, neighbour);
                     //*
-                    if (tempGScore < (gScore.ContainsKey(neighbor) ? gScore[neighbor] : Mathf.Infinity))
+                    if (tempGScore < (gScore.ContainsKey(neighbour) ? gScore[neighbour] : Mathf.Infinity))
                     {
                         // If true this path is better than any previous one
-                        cameFrom[neighbor] = current;
-                        gScore[neighbor] = tempGScore;
-                        fScore[neighbor] = gScore[neighbor] + Score(neighbor, nodeTarget);
-                        if (!nodesToExpand.Contains(neighbor))
-                            nodesToExpand.Add(neighbor);
+                        cameFrom[neighbour] = current;
+                        gScore[neighbour] = tempGScore;
+                        fScore[neighbour] = gScore[neighbour] + Score(neighbour, nodeTarget);
+                        if (!nodesToExpand.Contains(neighbour))
+                            nodesToExpand.Add(neighbour);
                     }//*/
                 }//*/
             }
@@ -141,60 +125,57 @@ namespace ByCubed7.Pathfinding
             return null;
         }
 
+        // Converts the dictionary node path to a list of nodes
+        private List<(int, int)> MakePath(Dictionary<(int, int), (int, int)> nodes, (int, int) targetNode)
+        {
+            (int, int) current = targetNode;
+            List<(int, int)> path = new List<(int, int)>() {current};
+
+            List<(int, int)> nodeKeys = new List<(int, int)>(nodes.Keys);
+            while (nodeKeys.Contains(current))
+            {
+                current = nodes[current];
+                path.Add(current);
+            }
+            return path;
+        }
+
         // The heuristic function
-        public static int Score(Node node, Node nodeTarget)
+        public static int Score((int, int) node, (int, int) nodeTarget)
         {
             float score = 0f;
             //score += node.distance;
-            score += node.Distance(nodeTarget);// * heuristic;
+            score += Distance(node, nodeTarget);// * heuristic;
             return (int)score;
         }
 
-        public Node NodeAtWorldPosition(Vector2 position, float dec = 0.5f)
+        public void AddNode(int x, int y)
         {
-            foreach (KeyValuePair<(int, int), Node> entry in nodes)
-            {
-                Vector2 difference = position - entry.Value.position;
-                if (difference.x + difference.y < dec) return entry.Value;
-            }
+            nodes[(x,y)] = new List<(int, int)>();
+        }
+
+        public List<(int, int)> GetNode((int, int) node)
+        {
+            if (nodes.ContainsKey(node)) return nodes[node];
             return null;
         }
+        public List<(int, int)> GetNode(int x, int y) => GetNode((x, y));
 
-        public Node NearestNodeAtWorldPosition(Vector2 position)
+        public void AddNeighbour((int, int) node, (int, int) neigh)
         {
-            float nearestDistance = 0f;
-            Node nearestNode = null;
-            foreach (KeyValuePair<(int, int), Node> entry in nodes)
-            {
-                float valueDistance = entry.Value.Distance(position);
-                if (valueDistance < nearestDistance || nearestNode == null) {
-                    nearestDistance = valueDistance;
-                    nearestNode = entry.Value;
-                }
-            }
-            return nearestNode;
+            nodes[node].Add(neigh);
         }
+        public void AddNeighbour(int x1, int y1, int x2, int y2) => AddNeighbour((x1, y1), (x2, y2));
 
-        public void DebugDraw()
-        {
-            foreach (KeyValuePair<(int, int), Node> entry in nodes)
-                entry.Value.DebugDraw();
+        public void GetCost((int, int) node) {}
 
+        static public int Distance((int, int) nodeFrom, (int, int) nodeTo) {
+            int changeInX = Mathf.Abs(nodeTo.Item1 - nodeFrom.Item1);
+            int changeInY = Mathf.Abs(nodeTo.Item2 - nodeFrom.Item2);
+            int stability = Mathf.Abs(changeInX - changeInY);
+
+            return (changeInX + changeInY) + stability / 2;
         }
-
-        public void DebugDrawPath(List<Node> path)
-        {
-            foreach (Node node in path)
-                node.DebugDraw(Color.green);
-
-            Node lastNode = null;
-            foreach (Node node in path)
-            {
-                if (lastNode != null)
-                    Debug.DrawLine(node.position, lastNode.position, Color.blue);
-                lastNode = node;
-            }
-        }
-
     }
+
 }
